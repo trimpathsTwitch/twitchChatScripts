3000da2d5934e8dfe9aec85999bd69b98f30102c
Dustin Yost <dustin.yost.t@gmail.com>
2021-03-27 15:16:19 -0400

    Add propoganda command first draft

--- propoganda.js
+++ propoganda.js
@@ -1,5 +1,18 @@
-var options = [
-  [ "Option 1", 5 ],
-  [ "Option 2", 10 ]
+const responses = [
+    [ "$(user) did you know Trimpaths claims to be 5' tall when they are really 4'11.75\"?", 30 ],
+    [ "PunOko", 10 ],
+    [ "BibleThump Trim is 5 ft tall BibleThump I'm not being threatened at all to say this BibleThump", 1 ]
 ];
-options[0];
+const total = responses.reduce((option1, option2) => option1[1] + option2[1], 0);
+var pickScore = Math.floor(Math.random() * total);
+var respondMsg = "i did something wrong o no";
+for (var i = 0; i < responses.length; ++i)
+{
+  if (pickScore < responses[i][1])
+  {
+    respondMsg = responses[i][0];
+    break;
+  }
+  pickScore -= responses[i][1];
+}
+respondMsg;
