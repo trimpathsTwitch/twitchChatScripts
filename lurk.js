78411ba8241386635b224c2f2536b388d6567884
Dustin Yost <dustin.yost.t@gmail.com>
2021-03-30 20:28:24 -0400

    Fix lurk?

--- lurk.js
+++ lurk.js
@@ -1,4 +1,3 @@
-var respondMsg = "i did something wrong o no";
 const responses = [
 	[ 'Have fun whatever you gonna do while lurking!', 45 ],
 	[ 'Have a lovely lurk!', 45 ],
@@ -6,6 +5,7 @@ const responses = [
 ];
 const total = responses.reduce((total, option) => total + option[1], 0);
 var pickScore = Math.floor(Math.random() * total);
+var respondMsg = "i did something wrong o no";
 for (var i = 0; i < responses.length; ++i)
 {
 	if (pickScore < responses[i][1])
