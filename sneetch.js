301cdbf94485471f0180d588334dd5174d43294b
Dustin Yost <dustin.yost.t@gmail.com>
2021-04-05 20:09:34 -0400

    Add sneetch script mark1

---
+++ sneetch.js
@@ -0,0 +1,19 @@
+let sneetches = ['plain-bellied', 'star-bellied'];
+let sneetchType = sneetches[Math.floor(Math.random() * 2)];
+
+const responses = [
+  [ `We love ${sneetchType} sneetches here, and we are pleased to count ${params.user} is among them! trimpaLove`, 10 ],
+];
+const total = responses.reduce((total, option) => total + option[1], 0);
+var pickScore = Math.floor(Math.random() * total);
+var respondMsg = "i did something wrong o no";
+for (var i = 0; i < responses.length; ++i)
+{
+	if (pickScore < responses[i][1])
+	{
+		respondMsg = responses[i][0];
+		break;
+	}
+	pickScore -= responses[i][1];
+}
+respondMsg;
