3e6b25090eb6075286f777da8c592f0badc4b753
Dustin Yost <dustin.yost.t@gmail.com>
2021-04-05 20:26:10 -0400

    Change propaganda emotes

--- propaganda.js
+++ propaganda.js
@@ -1,6 +1,6 @@
 const responses = [
 	[ "Did you know Trimpaths claims to be 5' tall when a doctor confirmed they are only 4'11\"? trimpaAngy trimpaAngy trimpaAngy", 30 ],
-	[ "PunOko trimpaAngy", 10 ],
+	[ "PowerUpL trimpaAngy PowerUpR", 10 ],
 	[ "BibleThump Trim is 5 ft tall BibleThump I'm not being threatened at all to say this BibleThump", 1 ]
 ];
 const total = responses.reduce((total, option) => total + option[1], 0);
