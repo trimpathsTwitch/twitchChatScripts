8802e9af390bfe3e84c822f71cfd0450d5c76685
Dustin Yost <dustin.yost.t@gmail.com>
2021-04-05 20:39:48 -0400

    Add propaganda quote

--- propaganda.js
+++ propaganda.js
@@ -1,7 +1,8 @@
 const responses = [
 	[ "Did you know Trimpaths claims to be 5' tall when a doctor confirmed they are only 4'11\"? trimpaAngy trimpaAngy trimpaAngy", 30 ],
 	[ "PowerUpL trimpaAngy PowerUpR", 10 ],
-	[ "BibleThump Trim is 5 ft tall BibleThump I'm not being threatened at all to say this BibleThump", 1 ]
+	[ "BibleThump Trim is 5 ft tall BibleThump I'm not being threatened at all to say this BibleThump", 1 ],
+  [ "\"I don't have a lot to give vertically, but I make up for it in volume\" - TrimPaths", 10 ],
 ];
 const total = responses.reduce((total, option) => total + option[1], 0);
 var pickScore = Math.floor(Math.random() * total);
